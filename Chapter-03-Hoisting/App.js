36ed3ee806558af2ea71232c230111545d9d0918
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2024-01-17 20:20:11 +0530

    Update App.js

--- Chapter-03-Hoisting/App.js
+++ Chapter-03-Hoisting/App.js
@@ -14,3 +14,21 @@ console.log(getName);
 var getName = function () {
     console.log('Namaste JavaScript');
 };
+
+
+// Example 2:
+getName(); // Namaste JavaScript
+console.log(x); // Uncaught Reference: x is not defined.
+console.log(getName); // f getName(){ console.log("Namaste JavaScript); }
+function getName() {
+    console.log('Namaste JavaScript');
+}
+
+
+// Try to understand by yourself
+// Example 3:
+getName(); // Uncaught TypeError: getName is not a function
+console.log(getName);
+var getName = function () {
+    console.log('Namaste JavaScript');
+};
