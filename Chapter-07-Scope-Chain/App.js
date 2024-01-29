4d99d9282bfccb55caf07b1d2789bdeed5581b8b
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2024-01-29 20:54:24 +0530

    Update App.js

--- Chapter-07-Scope-Chain/App.js
+++ Chapter-07-Scope-Chain/App.js
@@ -1,17 +1,39 @@
-function a() {
+function a() { // a is lexically inside global scope
   console.log(b); // undefined
 }
 var b = 10;
 
 a();
 
-function aa() {
+function aa() { //aa lexically inside the global scope
   c()
-  function c(){
+  function c(){ // c is lexically inside the aa function
     console.log(d);
   }
 }
 
-var b = 10;
+var d = 10;
 
 aa() // print 10 
+
+// here comes the scope chain
+function x(){  // x is lexocally insdie the global scope
+  y()
+  var z = 10;
+  function y() { // y is lexically inside x function
+    
+  }
+}
+x();
+// console.log(z);// here we will get error
+
+
+function parent(){
+  var num = 0;
+  child()
+  function child(){
+    console.log(num);  // prints 10 how it fincd the local memroy inside the child() function it its not finds it goes to referents of parent function and finds num insdie the parent
+  }
+}
+
+parent();
